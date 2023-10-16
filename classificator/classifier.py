0010c7290f55d2df5f093f77a8e6c6fcdeab2bb0
orisborlov <mrwiggle40000@gmail.com>
2023-10-16 09:38:20 +0700

    reforked classifier to mongo

--- classificator/classifier.py
+++ classificator/classifier.py
@@ -1,44 +1,58 @@
 import os
+
+from pymongo import MongoClient
 from classificator.dtw import Dtw
-from classificator.testing import askii
+
+# Provide the mongodb atlas url to connect python to mongodb using pymongo
+CONNECTION_STRING = "mongodb://localhost:27017"
+
+# Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+client = MongoClient(CONNECTION_STRING)
 
 
 class Classifier:
-    def __init__(self):
-        self.anger_path = os.environ.get('PATTERNS_ANGER_FILE'),
-        self.happyness_path = os.environ.get('PATTERNS_HAPPYNESS_FILE'),
-        self.calm_path = os.environ.get('PATTERNS_CALM_FILE'),
-        self.disgust_path = os.environ.get('PATTERNS_DISGUST_FILE'),
-        self.fear_path = os.environ.get('PATTERNS_FEAR_FILE')
+    def __init__(self, level):
+        anger_array = client['patterns'][str(level)].find({'emotion': 'anger'})
+        self.anger_patterns = [obj['pattern'] for obj in anger_array]
 
+        calm_array = client['patterns'][str(level)].find({'emotion': 'calm'})
+        self.calm_patterns = [obj['pattern'] for obj in calm_array]
 
-    def classify(self):
-        anger = self.check_min(self.anger_file, askii(input))
+        happy_array = client['patterns'][str(level)].find({'emotion': 'happy'})
+        self.happy_patterns = [obj['pattern'] for obj in happy_array]
+
+        disgust_array = client['patterns'][str(level)].find({'emotion': 'disgust'})
+        self.disgust_patterns = [obj['pattern'] for obj in disgust_array]
+
+        fear_array = client['patterns'][str(level)].find({'emotion': 'fear'})
+        self.fear_patterns = [obj['pattern'] for obj in fear_array]
+
+    def classify(self, input_pattern):
+        anger = self.check_min(self.anger_patterns, askii(input_pattern))
         print('Злость:' + str(anger))
-        happyness =self.check_min(self.happyness_file, askii(input))
+        happyness =self.check_min(self.happy_patterns, askii(input_pattern))
         print('Радость:' + str(happyness))
-        calm = self.check_min(self.calm_file, askii(input))
+        calm = self.check_min(self.calm_patterns, askii(input_pattern))
         print('Спокойствие:' + str(calm))
-        disgust = self.check_min(self.disgust_file, askii(input))
+        disgust = self.check_min(self.disgust_patterns, askii(input_pattern))
         print('Отвращение:' + str(disgust))
-        fear = self.check_min(self.fear_file, askii(input))
+        fear = self.check_min(self.fear_patterns, askii(input_pattern))
         print('Страх:' + str(fear))
         return self.find_min_var_name(anger, happyness, calm, disgust, fear)
         
 
-    def check_min(path, inputPattern):
-        min = 10000
-        f = open(path, 'r')
-        lines = f.read().splitlines()
-        for line in lines:
-            line = askii(line)
-            temp = Dtw(inputPattern, line).getDistance()
+    def check_min(self, patterns_array, inputPattern):
+        min = 100000
+        
+        for pattern in patterns_array:
+            pattern = askii(pattern)
+            temp = Dtw(inputPattern, pattern).getDistance()
             if temp < min:
                 min = temp
-        f.close
+        
         return (min)
 
-    def find_min_var_name(a, b, c, d, e):
+    def find_min_var_name(self, a, b, c, d, e):
         smallest = a
         if b < smallest:
             smallest = b
@@ -58,3 +72,9 @@ class Classifier:
             return "disgust"
         else:
             return "fear"
+
+def askii(text):
+    ascii_values = []
+    for character in text:
+        ascii_values.append(ord(character))
+    return ascii_values
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from classificator.classifier import Classifier
 
 
-classifier = Classifier()
+
+classifier = Classifier(5)
+print(classifier.classify('2 2 1 1 0 0 0 3 3 0 1 0 3 3 0'))
+    # Далее можно работать с результатом
+
+
